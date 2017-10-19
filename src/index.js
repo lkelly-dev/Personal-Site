4e178b01d67d12b196885eed4ad58ed0f7fc1ada
Liam Kelly <me@liamckelly.com>
2017-10-19 17:05:41 -0400

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./routes";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
-registerServiceWorker();
